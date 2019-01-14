2969e1fee865cb974d51f80089fadbac349c7b31
Your Name <you@example.com>
2019-01-14 21:06:58 +0000

    draft

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+var EasyI2C = require("easyi2c");
+
+var i2c = new EasyI2C(1);
 var OctoPrint = require('octo-client');
+var mylcd;
+
+function connectLcd() {
+return new Promise(function(done){
+i2c.on("connectedDevice", e => {
+console.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    console.log(`${ e.fid} has connected to the I2C-Bus`);
+    if(e.fid =="0x27") {
+        lcd = i2c.LCD(e.id, 16,4);
+        //lcd.print("Hello World");
+        mylcd=lcd;
+    } 
+    done();
+});
+i2c.on("ready", () => {
+    console.log("EasyI2C is ready!");
+});
+i2c.start();
+});
+
+}
 
 
 //OctoPrint.version(function(response) {                                 console.log(response); });
@@ -39,18 +63,38 @@ function jobStatus() {
 function printerState() {
 	return new Promise(function(done){
 		OctoPrint.printerState(function(response){
-		  line4 = response.temperature.bed.actual + response.temperature.bed.target+"C" + " " +response.temperature.tool0.actual + response.temperature.tool0.target+"C";
+		  line4 = response.temperature.bed.actual + "/" + response.temperature.bed.target+"C" + " " +response.temperature.tool0.actual + "/" + response.temperature.tool0.target+"C";
 		  done();
 		});
 	});
 }
 
-jobStatus()
-.then(printerState)
+
+function srtL(s) {
+	s = s + "                                           ";
+	return s.substring(0,20);
+}
+
+connectLcd()
 .then(function(){
-	console.info(line1);
-	console.info(line2);
-	console.info(line3);
-	console.info(line4);
-	
+	setInterval(function(){
+		jobStatus()
+		.then(printerState)
+		.then(function(){
+			console.info(line1);
+			console.info(line2);
+			console.info(line3);
+			console.info(line4);
+      	 		mylcd.setCursor(0,0);
+      	 		mylcd.print(srtL(line1));
+      	 		mylcd.setCursor(0,1);
+       			mylcd.print(srtL(line2));
+       			mylcd.setCursor(0,2);
+       			mylcd.print(srtL(line3));
+       			mylcd.setCursor(0,3);
+       			mylcd.print(srtL(line4));
+       			mylcd.on();	
+		});
+	}, 5000);
+
 });
